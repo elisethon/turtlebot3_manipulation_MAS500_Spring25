b02126aade65d67a191c5ca86a4725cb3b7b5f59
elisethon <145268793+elisethon@users.noreply.github.com>
2025-05-28 13:02:43 +0200

    Added more comments
    Signed-off-by: elisethon <145268793+elisethon@users.noreply.github.com>

--- src/analog_sensor/analog_sensor/adc_publisher.py
+++ src/analog_sensor/analog_sensor/adc_publisher.py
@@ -1,3 +1,9 @@
+# This script is made to be able to read the analog signal from the sensor using:
+# 12 bit ADC from Adafruit 
+# Adafruit Industries LLC 4965 analog sensor 
+
+# ROS2 is used to publish the readings 
+
 import time
 import rclpy  # ROS 2 Python client library
 from rclpy.node import Node
@@ -6,48 +12,46 @@ import Adafruit_ADS1x15  # ADC Library
 
 class AnalogSensorPublisher(Node):
     def __init__(self):
-        # Ensure Node is properly initialized with a name
+        # Initialized node
         super().__init__("analog_sensor_publisher")  
 
-        # Create a ROS 2 publisher
+        # Create publisher
         self.publisher_ = self.create_publisher(Float32, "read_analog_sensor", 10) # Float32
 
         # Initialize ADC (ADS1015)
         self.adc = Adafruit_ADS1x15.ADS1015()
         self.channel = 0  # Sensor connected to A0
-        self.gain = 2  # ±2.048V FSR# 1  # ±4.096V FSR
+        self.gain = 2  # for ±2.048V FSR, set to 1 if ±4.096V FSR
         
-        self.first_publish = True  # <- Flag for first-time log
-
-        # Timer to publish data every second
+        self.first_publish = True  
         self.timer = self.create_timer(1.0, self.publish_sensor_data)
-    
-    def convert_to_arduino_scale(self, adc_value):
-        """Will convert to voltage"""
-        return (adc_value)*(2.048/2047) # adc_value*0.3584 #0.6274
 
+    # CONVERT VALUE TO VOLT
+    def convert_to_arduino_scale(self, adc_value):
+        """Will convert analog signal to voltage"""
+        return (adc_value)*(2.048/2047)
+
+    # READ VALUE
     def publish_sensor_data(self):
-        # Read ADC value
         raw_value = self.adc.read_adc(self.channel, gain=self.gain)
 
-        # Convert to Arduino-equivalent value
+        # Convert value
         corrected_value = self.convert_to_arduino_scale(raw_value)
 
-        # Publish message
+        # Publish
         msg = Float32()
         msg.data = corrected_value
         self.publisher_.publish(msg)
 
+        # Reccomend using 'ros2 topic echo /read_analog_sensor' to see values, but if desired to log, uncomment next line: 
         # self.get_logger().info(f"Published: {corrected_value:.2f}")
         
         if self.first_publish:
             self.get_logger().info("Publishing analog sensor signal")
-            self.first_publish = False  # <- Prevent further logs
+            self.first_publish = False  
 
 def main(args=None):
     rclpy.init(args=args)
-    
-    # Ensure Node is properly initialized
     node = AnalogSensorPublisher()
 
     try:
